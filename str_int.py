109ad386cf01ffc53ffe787ef6b291b09f2726c6
miraashri <suganthamiraa.it13@bitsathy.ac.in>
2016-06-08 11:03:00 +0530

    Create str_int.py

---
+++ str_int.py
@@ -0,0 +1,14 @@
+def main():
+    string = (raw_input("Enter the string : "))
+    integer = str_to_int(string)
+    print("The string and the integer are : %s and %d"%(string,integer))
+
+def str_to_int(ex):
+    counter = ans = 0
+    for i in reversed(ex):
+        ans += (ord(i) - 48) * 10 ** counter
+        counter += 1
+    return ans
+
+if __name__ == '__main__':
+    main()
