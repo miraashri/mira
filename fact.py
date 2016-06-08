82abba50b15f61708aa3d32cc81c7d69c5165084
miraashri <suganthamiraa.it13@bitsathy.ac.in>
2016-06-08 10:21:17 +0530

    Create fact.py

---
+++ fact.py
@@ -0,0 +1,12 @@
+def factorial():
+    num=int(input("Enter the factorial:"))
+    fact=1
+    if(num<0):
+       print(number is not factorial)
+    elif(num==0): 
+       print("number 0 is factorial")
+    else:
+       for i in range(1,num+1):
+         fact=fact*i
+       print("The factorial of",num ,"is",fact)
+factorial()
